d2bd019a98dfd75ecbe0d5c38578a9acea4b401a
thehue <gouk32@naver.com>
2024-09-14 21:36:40 +0900

    1.1.1 <canvas> 요소의 크기와 드로잉 표면의 크기

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./page.module.css";
 import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvas2Ref = useRef<HTMLCanvasElement | null>(null);
+  const canvas3Ref = useRef<HTMLCanvasElement | null>(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
+  const canvasHandler = (canvasElement: HTMLCanvasElement | null) => {
+    const canvas = canvasElement;
     if (!canvas) return;
 
     const context = canvas.getContext("2d");
@@ -25,11 +27,39 @@ export default function Home() {
       canvas.width / 2 - 150,
       canvas.height / 2 + 15
     );
+  };
+
+  useEffect(() => {
+    canvasHandler(canvasRef.current);
+    canvasHandler(canvas2Ref.current);
+    canvasHandler(canvas3Ref.current);
   }, []);
 
   return (
-    <canvas className={styles.canvas} width={600} height={300} ref={canvasRef}>
-      Canvas not supported
-    </canvas>
+    <>
+      <h3>
+        Canvas Element Size: 600 x 300, Canvas Drawing Surface Size: 600 x 300
+      </h3>
+      <canvas
+        className={styles.common}
+        width={600}
+        height={300}
+        ref={canvasRef}
+      >
+        Canvas not supported
+      </canvas>
+      <h3>
+        Canvas Element Size: 300 x 150, Canvas Drawing Surface Size: 300 x 150
+      </h3>
+      <canvas className={styles.common} ref={canvas2Ref}>
+        Canvas not supported
+      </canvas>
+      <h3>
+        Canvas Element Size: 600 x 300, Canvas Drawing Surface Size: 300 x 150
+      </h3>
+      <canvas className={`${styles.common} ${styles.canvas2}`} ref={canvas3Ref}>
+        Canvas not supported
+      </canvas>
+    </>
   );
 }
